c527ff30fee57d0040b1281cce38f133e4cf7b8d
Priya041199 <priyagawde1712@gmail.com>
2024-06-10 16:42:12 +0530

    Delete cloud_demo_project2 directory

--- cloud_demo_project2/dist/controller/App.controller.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(o){"use strict";return o.extend("com.ibspl.clouddemoproject2.controller.App",{onInit:function(){}})});
-//# sourceMappingURL=App.controller.js.map
