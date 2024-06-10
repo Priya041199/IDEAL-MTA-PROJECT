2c2123294585e2c86429b8fc1712a74c833073e1
Priya041199 <priyagawde1712@gmail.com>
2024-06-10 15:07:23 +0530

    Delete demo_project directory

--- demo_project/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["com/ibspl/demoproject/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- demo_project/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"comibspl/demo_project/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
