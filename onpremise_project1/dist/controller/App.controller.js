0a77bffb4f2480940341200b2e860c7f850670bd
Priya041199 <priyagawde1712@gmail.com>
2024-06-10 16:44:40 +0530

    Delete onpremise_project1 directory

--- onpremise_project1/dist/controller/App.controller.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(e){"use strict";return e.extend("com.ibspl.onpremiseproject1.controller.App",{onInit:function(){}})});
-//# sourceMappingURL=App.controller.js.map
